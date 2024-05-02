79ed689c8fe314b6818b2660da857be756715521
Ethan Tracey <ethantracey@Ethans-MBP.hitronhub.home>
2024-05-02 13:42:50 -0230

    added canvas Added a canvas to the screen and placeholder title. Started player constructor and minor styling

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('Hello World')
+// init canvas
+var canvas = document.getElementById('main-canvas');
+var ctx = canvas.getContext("2d");
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+// init player
+class Player {
+    constructor(x, y, colour) {
+        this.x = x
+        this.y = y
+        this.colour = colour
+    }
+}
